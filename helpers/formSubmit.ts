5b8c5d138fb697e22431528eb8b768313300468f
rafaelpradella <pradella@outlook.com>
2022-10-24 15:38:43 +0100

    Splitting functions + adding currying

--- helpers/formSubmit.ts
+++ helpers/formSubmit.ts
@@ -1,5 +1,6 @@
 import { SyntheticEvent } from "react";
-import { isRight } from "fp-ts/lib/Either";
+import * as E from "fp-ts/lib/Either";
+import * as A from "fp-ts/Array";
 
 import type { ValidatorType } from '../components/Field';
 
@@ -17,33 +18,44 @@ const getValidatorKeys = (validators: ValidatorType): Array<string> => {
     return Object.keys(validators);
 };
 
-const hasAllRequiredBeenFilled = (keys: Array<string>, formData: FormDataMatrix) => keys.every(key => 
-    formData.some((matrix: FormDataMatrix[0]) => matrix[0] === key && !!matrix[1]));
+const hasAllRequiredBeenFilled = (formData: FormDataMatrix) =>
+    (keys: Array<string>) => 
+        keys.every(key => 
+            formData.some((matrix: FormDataMatrix[0]) => matrix[0] === key && !!matrix[1]));
 
-const isFieldRequired = (fieldId: string, keys: Array<string>) => 
-    keys.some(key => key === fieldId);
+const isFieldRequired = (keys: Array<string>) => 
+    (fieldId: string) =>
+        keys.some(key => key === fieldId);
 
 const errorsOnRequiredFields = (validators: ValidatorType, formData: FormDataMatrix): ErrorsList => formData.reduce(
     (acc: ErrorsList, field: FormDataMatrix[0]) => {
         const [ fieldId, fieldValue ] = field;
         const validatorKeys = getValidatorKeys(validators);
 
-        if(!isFieldRequired(fieldId, validatorKeys)) return acc;
+        if(!isFieldRequired(validatorKeys)(fieldId)) return acc;
 
         const checkValidity = validators[fieldId](fieldValue);
 
-        if(isRight(checkValidity)) return acc;
+        if(E.isRight(checkValidity)) return acc;
         
         acc.push({ fieldId, message: checkValidity.left });
         return acc;
 }, []);
 
-const getFeedbackString = (errorsList: ErrorsList): string => {
-    if(!errorsList?.length) return 'Everything is fine, everything is cool 😎';
+const generateErrorStrings = (errorsList: ErrorsList): string => {
+    const ERROR_TITLE = 'Fix those issues before submitting: \n\n';
 
-    return errorsList?.reduce((acc, err) =>
+    return errorsList.reduce((acc, err) => 
         acc += `${err.fieldId}: ${err.message} \n`
-    , 'Fix those issues before submitting: \n\n')
+    , ERROR_TITLE)
+}
+
+const getFeedbackString = (errorsList: ErrorsList): string => {
+    const SUCCESS_MSG = 'Everything is fine, everything is cool 😎';
+
+    return errorsList?.length 
+        ? generateErrorStrings(errorsList)
+        : SUCCESS_MSG;
 }
 
 export const handleSubmit = (e: FormEvent, requiredValidators: any): void => {
@@ -52,7 +64,7 @@ export const handleSubmit = (e: FormEvent, requiredValidators: any): void => {
     const validatorKeys = getValidatorKeys(requiredValidators);
     const formMatrix: FormDataMatrix = getFormData(e);
 
-    if(!hasAllRequiredBeenFilled(validatorKeys, formMatrix))
+    if(!hasAllRequiredBeenFilled(formMatrix)(validatorKeys))
         return alert('Please fill all required fields');
 
     const errorsList: ErrorsList = errorsOnRequiredFields(requiredValidators, formMatrix);
