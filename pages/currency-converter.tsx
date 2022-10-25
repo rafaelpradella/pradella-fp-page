df7bcc18aa4aa112f23cad7abbc4e3c7a7a9c1e8
rafaelpradella <pradella@outlook.com>
2022-10-25 14:15:49 +0100

    [wip] Splitting logic to service file

--- pages/currency-converter.tsx
+++ pages/currency-converter.tsx
@@ -1,48 +1,24 @@
 import type { NextPage } from 'next'
-import { pipe } from 'fp-ts/lib/function';
-import * as A from 'fp-ts/Array';
 
+import CurrencyService from '../services/currency';
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.scss'
 
-const TOP_NOTCH_CURRENCIES = ['EUR', 'GBP', 'AUD', 'NZD', 'USD', 'CAD', 'CHF', 'JPY'];
+
 
 export async function getStaticProps() {
-	
-	const isTopCurrency = (arr) => {
-		return TOP_NOTCH_CURRENCIES.some((topSymbol) => {
-			return topSymbol === arr[0];
-		})
-	};
-
-	let myHeaders = new Headers();
-	myHeaders.append("apikey", process.env.FIXER_API_KEY);
-
-	const requestOptions = {
-		method: 'GET',
-		redirect: 'follow',
-		headers: myHeaders
-	};
-
-	const symbolsRequest = await fetch("https://api.apilayer.com/fixer/symbols", requestOptions);
-	const res = await symbolsRequest.json();
-
-	const filterCurrencies = (currencies, predicate) => pipe(currencies,
-			Object.entries,
-			A.partition(predicate)
-	);
-
-	const filteredByImportance = filterCurrencies(res?.symbols, isTopCurrency);
+	const currencyRes = await CurrencyService.fetchCurrenciesListByRelevance();
 
 	return {
 	  props: {
-			topCurrencies: filteredByImportance.right,
-			otherCurrencies: filteredByImportance.left,
+			topCurrencies: currencyRes.right,
+			otherCurrencies: currencyRes.left,
 	  },
 	}
 }
 
 export default function CurrencyConverter({ topCurrencies, otherCurrencies }) {
+	
 	const generateOptions = (currencies: Array<[string, string]>) => 
 		currencies.map((currency, i) => (
 			<option value={currency[0]} key={i}>{`${currency[0]} - ${currency[1]}`}</option>
---
+++ services/currency.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosResponse } from 'axios';
+import { pipe } from 'fp-ts/lib/function';
+import { isLeft } from 'fp-ts/lib/Either';
+import * as TE from 'fp-ts/TaskEither';
+import * as A from 'fp-ts/Array';
+
+type CurrencyResponse = { symbols: CurrencyData, status: string };
+type CurrencyData = { [key: string] : string };
+type CurrencyMatrix = [string, string][];
+
+const TOP_NOTCH_CURRENCIES = ['EUR', 'GBP', 'AUD', 'NZD', 'USD', 'CAD', 'CHF', 'JPY'] as const;
+
+const fixerClient = axios.create({
+  baseURL: "https://api.apilayer.com/fixer/",
+  headers: { apiKey: process.env.FIXER_API_KEY},
+  timeout: 2000,
+})
+
+const isTopCurrency = (key: string) => {
+  return TOP_NOTCH_CURRENCIES.some((topSymbol) => {
+    return topSymbol === key;
+  })
+};
+
+const listByRelevanceTier = (currencies, predicate) => pipe(currencies,
+  Object.entries,
+  A.partition(predicate)
+);
+
+const fetchCurrenciesList = async() => {
+  const currencyList: TE.TaskEither<Error, CurrencyData> = await pipe(
+    TE.tryCatch(
+      () => fixerClient.get('symbols'),
+      (err) => new Error(`${err}`)
+    ),
+    TE.map((res: AxiosResponse<CurrencyResponse>) => res.data.symbols),
+  )()
+
+  return currencyList;
+}
+
+
+const fetchCurrenciesListByRelevance = async() => {
+  const getListData = await fetchCurrenciesList();
+  
+  return pipe(getListData,
+    TE.getOrElse((err) => console.error(err)),
+    TE.chainW(
+      isTopCurrency,
+    ),
+  )
+}
+
+const CurrencyService = {
+  fetchCurrenciesList,
+  fetchCurrenciesListByRelevance,
+};
+
+export default CurrencyService;
+
