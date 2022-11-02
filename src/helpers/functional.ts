936e78c3d423ecfd831699c8250ee3a282eef1be
rafaelpradella <pradella@outlook.com>
2022-11-02 22:19:32 +0000

    Removing redundant types + simplify lift fn

--- src/helpers/functional.ts
+++ src/helpers/functional.ts
@@ -2,12 +2,9 @@ import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/lib/function';
 import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
 
-export type LiftedEither<E, A> = E.Either<NonEmptyArray<E>, A>;
-
-export function lift<E, A>(check: (a: A) => E.Either<E, A>): (a: A) => E.Either<NonEmptyArray<E>, A> {
-    return a =>
-        pipe(
-            check(a),
-            E.mapLeft(a => [a])
-        )
-}
+export const lift = <E, A>(check: (a: A) => E.Either<E, A>) => 
+	(a: A): E.Either<NonEmptyArray<E>, A> =>  
+			pipe(a,
+				check,
+				E.mapLeft(a => [a])
+			)
--- src/helpers/validators.ts
+++ src/helpers/validators.ts
@@ -3,43 +3,41 @@ import { pipe } from 'fp-ts/function';
 import { sequenceT } from 'fp-ts/lib/Apply';
 import { getSemigroup, ReadonlyNonEmptyArray } from 'fp-ts/lib/ReadonlyNonEmptyArray';
 
-import { lift, LiftedEither } from './functional';
-
-type ValidationReturn = E.Either<string, string>
+import { lift } from './functional';
 
 const MIN_LENGTH = 6;
 const ERROR_MSG = {
-    NO_CONTENT: 'Fill the field',
-    LENGTH: 'At least 6 characters',
-    CAPITAL_LETTER: 'Include 1 capital letter',
-    NUMBER: 'At least 1 number',
+	NO_CONTENT: 'Fill the field',
+	LENGTH: 'At least 6 characters',
+	CAPITAL_LETTER: 'Include 1 capital letter',
+	NUMBER: 'At least 1 number',
 } as const;
 
-const hasContent = (s: string): ValidationReturn =>
-    (s?.replaceAll(/\s/g, '')?.length > 0) ? E.right(s) : E.left(ERROR_MSG.NO_CONTENT)
+const hasContent = (s: string) =>
+	(s?.replaceAll(/\s/g, '')?.length > 0) ? E.right(s) : E.left(ERROR_MSG.NO_CONTENT)
 
-const minLenght = (s: string): ValidationReturn =>
-    (s?.length > MIN_LENGTH) ? E.right(s) : E.left(ERROR_MSG.LENGTH)
+const minLenght = (s: string) =>
+	(s?.length > MIN_LENGTH) ? E.right(s) : E.left(ERROR_MSG.LENGTH)
 
-const oneCapital = (s: string): ValidationReturn =>
-    (/[A-Z]/g.test(s)) ? E.right(s) : E.left(ERROR_MSG.CAPITAL_LETTER)
+const oneCapital = (s: string) =>
+	(/[A-Z]/g.test(s)) ? E.right(s) : E.left(ERROR_MSG.CAPITAL_LETTER)
 
-const oneNumber = (s: string): ValidationReturn =>
-    (/[0-9]/g.test(s)) ? E.right(s) : E.left(ERROR_MSG.NUMBER)
+const oneNumber = (s: string) =>
+	(/[0-9]/g.test(s)) ? E.right(s) : E.left(ERROR_MSG.NUMBER)
 
-export const validateName = (s: string): E.Either<ReadonlyNonEmptyArray<string>, Array<string>> =>
-    pipe(
-        sequenceT(E.getApplicativeValidation(getSemigroup<string>()))(
-            lift(hasContent)(s),
-        )
-    )
+export const validateName = (s: string) =>
+	pipe(
+		sequenceT(E.getApplicativeValidation(getSemigroup<string>()))(
+			lift(hasContent)(s),
+		)
+	)
 
-export const validatePassword = (s: string): E.Either<ReadonlyNonEmptyArray<string>, Array<string>> =>
-    pipe(
-        sequenceT(E.getApplicativeValidation(getSemigroup<string>()))(
-            lift(minLenght)(s),
-            lift(oneCapital)(s),
-            lift(oneNumber)(s),
-        )
-    )
+export const validatePassword = (s: string) =>
+	pipe(
+		sequenceT(E.getApplicativeValidation(getSemigroup<string>()))(
+			lift(minLenght)(s),
+			lift(oneCapital)(s),
+			lift(oneNumber)(s),
+		)
+	)
 
