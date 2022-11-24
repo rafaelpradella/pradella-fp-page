bcdd0470fb529c27a9672ad4934060c4346a52f8
rafaelpradella <pradella@outlook.com>
2022-11-24 13:49:11 +0000

    Adding codec to video page color list

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -1,29 +1,53 @@
-import { url } from 'inspector';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/lib/function';
 import type { NextPage } from 'next'
-import { platform } from 'os';
-import Plyr from 'plyr-react';
 
-import Layout from '../components/Layout'
-import styles from '../styles/Home.module.scss'
+import { ColorHex, ColorHexCodec } from 'types/colors.codec';
+import Layout from 'components/Layout'
+import styles from 'styles/Home.module.scss';
 
 const Video: NextPage = () => {
-	
-	const PLAYER_SOURCE = {
-		type: 'video',
-		sources: [
-		  {
-			src: '76979871',
-			provider: 'vimeo',
-		  },
-		],
-	};
-	
+	const BG_PALETTE = ['#985b3c', '#fbcc98', '#692115'] as const;
+	const POUR_UNE_VOIX_ID = '8NOBBvQRT-w' as const;
+
+	const FoggyItem = ({ color } : { color: ColorHex }) => {
+		return (
+			<span style={{ backgroundColor: color }}>
+				{color}
+			</span>
+		)
+	}
+
+	const FoggyBackground = ({ colors }: { colors: ColorHex[] }) => pipe(colors,
+			O.fromNullable,
+			O.filter(list => list?.length > 1),
+			O.map(list => 
+				list.filter(hex => E.isRight(ColorHexCodec.decode(hex))
+			)),
+			O.fold(
+				() => (<code className='failed' />),
+				(hexList) => (
+					<code className={styles.foggyList}>
+						{hexList.map(hex => (<FoggyItem color={hex} key={hex} />))}
+					</code>
+				),
+			)
+		)
+
+	const PlyrTube = ({ id }: { id: string }) => {
+		return (
+			<span>{id}</span>
+		)
+	}
+
 	return (
 		<Layout>
+			<FoggyBackground colors={BG_PALETTE} />
 			<h1 className={styles.title}>
 				Relax and watch this:
 			</h1>
-			<Plyr source={PLAYER_SOURCE} />
+			<PlyrTube id={POUR_UNE_VOIX_ID} />
 		</Layout>
 	)
 }
