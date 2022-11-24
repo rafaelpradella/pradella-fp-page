91ed301a61eac151dd4e5f5548d99115149c1d44
rafaelpradella <pradella@outlook.com>
2022-11-24 22:26:26 +0000

    Using PLYR ref api to mess with video speed

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -1,16 +1,20 @@
 import * as E from 'fp-ts/Either';
+import * as IOP from 'fp-ts/IOOption'
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/lib/function';
 import type { NextPage } from 'next'
-import Plyr from 'plyr-react';
+import Plyr, { APITypes } from 'plyr-react';
 import "plyr-react/plyr.css";
 
 import { ColorHex, ColorHexCodec } from 'types/colors.codec';
-import Layout from 'components/Layout'
+import Layout from 'components/Layout';
 import styles from 'styles/Home.module.scss';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, SyntheticEvent, RefObject } from 'react';
+import { logPipe } from 'helpers/functional';
 
 const Video: NextPage = () => {
+	const plyrEl = useRef<APITypes>(null);
+
 	const BG_PALETTE = ['#985b3c', '#fbcc98', '#692115'];
 	const POUR_UNE_VOIX_ID = '8NOBBvQRT-w' as const;
 
@@ -47,16 +51,49 @@ const Video: NextPage = () => {
 	)
 
 	const PlyrTube = ({ id }: { id: string }) => {
-		const VIDEO_SRC = { type: 'video', sources: [
-			{ src: id, provider: "youtube" }
-		],}
+		const VIDEO_SRC = {
+			type: 'video', sources: [
+				{ src: id, provider: "youtube" }
+			],
+		}
 
 		return (
 			<div className={styles.videoFrame}>
-				<Plyr source={VIDEO_SRC}/>
+				<Plyr ref={plyrEl} source={VIDEO_SRC}>
+					<iframe
+						allowFullScreen
+						src={`https://www.youtube.com/embed/${id}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`}
+						allow="autoplay"
+					></iframe>
+				</Plyr>
 			</div>
 		)
 	}
+
+	const SpeedHandler = ({ videoRef } : { videoRef: RefObject<APITypes> }) => {
+		const [speedInfo, setSpeedInfo] = useState<number>(1);
+		const changePace = (ev: SyntheticEvent<HTMLInputElement>) => pipe(ev,
+			O.fromNullable,
+			O.map(ev => ev?.target?.valueAsNumber),
+			O.map(value => {
+				videoRef.current.plyr.speed = value;
+				setSpeedInfo(value);
+			}),
+		);
+
+		return (
+			<>
+				<input 
+					type="range"
+					min="0.5"
+					max="2"
+					step="0.25"
+					value={speedInfo}
+					onChange={ev => changePace(ev)}
+				/>
+				<span>Actual Speed: {speedInfo}x</span>
+			</>
+		)}
 
 	return (
 		<Layout>
@@ -65,6 +102,7 @@ const Video: NextPage = () => {
 				Relax and watch this:
 			</h1>
 			<PlyrTube id={POUR_UNE_VOIX_ID} />
+			<SpeedHandler videoRef={plyrEl} />
 		</Layout>
 	)
 }
