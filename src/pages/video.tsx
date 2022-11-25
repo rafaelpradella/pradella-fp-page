b200fd4060511952bb36bee927b833eae080cb23
rafaelpradella <pradella@outlook.com>
2022-11-25 16:30:31 +0000

    Adjusting default speed in recurrent views

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -1,7 +1,9 @@
+import { useState, useEffect, useRef, SyntheticEvent, RefObject } from 'react';
 import * as E from 'fp-ts/Either';
-import { IO } from 'fp-ts/IO';
-import { io } from 'fp-ts';
+import { getItem } from 'fp-ts-local-storage';
+import * as IO from 'fp-ts/IOOption';
 import * as O from 'fp-ts/Option';
+import { io } from 'fp-ts';
 import { pipe } from 'fp-ts/lib/function';
 import type { NextPage } from 'next'
 import Plyr, { APITypes } from 'plyr-react';
@@ -10,7 +12,6 @@ import "plyr-react/plyr.css";
 import { ColorHex, ColorHexCodec } from 'types/colors.codec';
 import Layout from 'components/Layout';
 import styles from 'styles/Home.module.scss';
-import { useState, useEffect, useRef, SyntheticEvent, RefObject } from 'react';
 import { logPipe } from 'helpers/functional';
 
 const Video: NextPage = () => {
@@ -19,13 +20,20 @@ const Video: NextPage = () => {
 	const BG_PALETTE = ['#985b3c', '#fbcc98', '#692115'];
 	const POUR_UNE_VOIX_ID = '8NOBBvQRT-w' as const;
 
-	const FoggyItem = ({ color }: { color: ColorHex }) => {
+	const getSavedPlyrSettings = (setting: string) => pipe(
+			getItem('plyr'),
+			IO.map(JSON.parse),
+			IO.map(obj => obj[setting]),
+			IO.map(res => parseFloat(res)),
+		);
+
+	const FoggyItem = ({ color, isPlaying }: { color: ColorHex, isPlaying: boolean }) => {
 		const [translateX, setTranslateX] = useState<number>(0);
 
 		useEffect(() => {
-			const shouldAnimate = window?.matchMedia('(prefers-reduced-motion: reduce)')?.matches;
+			const shouldAnimate = !isPlaying && !window?.matchMedia('(prefers-reduced-motion: reduce)')?.matches;
 
-			shouldAnimate && setTimeout(() => setTranslateX(Math.random() * 100 / 3), 1000);
+			shouldAnimate && setTimeout(() => setTranslateX(Math.random() * 100 / 3), 1500);
 		}, [translateX])
 
 		return (
@@ -36,6 +44,8 @@ const Video: NextPage = () => {
 	}
 
 	const FoggyBackground = ({ colors, videoRef }: { colors: Array<ColorHex>, videoRef: RefObject<APITypes> }) => {
+		const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
 		return pipe(colors,
 			O.fromNullable,
 			O.filter(list => list?.length >= 1),
@@ -46,7 +56,13 @@ const Video: NextPage = () => {
 				() => (<code className='failed' />),
 				(hexList) => (
 					<code className={styles.foggyList}>
-						{hexList.map(hex => (<FoggyItem color={hex} key={hex} />))}
+						{hexList.map(hex => (
+							<FoggyItem
+								isPlaying={isPlaying}
+								color={hex}
+								key={hex}
+							/>
+						))}
 					</code>
 				),
 			)
@@ -75,7 +91,17 @@ const Video: NextPage = () => {
 
 	const SpeedHandler = ({ videoRef }: { videoRef: RefObject<APITypes> }) => {
 		const [speedInfo, setSpeedInfo] = useState<number>(1);
-		const changePace = (ev: SyntheticEvent<HTMLInputElement>): IO<void> => pipe(ev,
+
+		useEffect(() => pipe('speed',
+			getSavedPlyrSettings,
+			logPipe('aaa'),
+			IO.match(
+				() => console.log('Starting w/ default speed: 1x'),
+				setSpeedInfo,
+			)
+		), [])
+
+		const changePace = (ev: SyntheticEvent<HTMLInputElement>) => pipe(ev,
 			O.fromNullable,
 			O.map(ev => ev?.target?.valueAsNumber),
 			O.fold(
@@ -95,17 +121,17 @@ const Video: NextPage = () => {
 					min="0.5"
 					max="2"
 					step="0.25"
-					defaultValue={1}
+					defaultValue={speedInfo}
 					onChange={ev => changePace(ev)}
 				/>
 				<span>Actual Speed: {speedInfo}x</span>
 			</>
-		)
+		);
 	}
 
 	return (
 		<Layout>
-			<FoggyBackground colors={BG_PALETTE} />
+			<FoggyBackground videoRef={plyrEl} colors={BG_PALETTE} />
 			<h1 className={styles.title}>
 				Relax and watch this:
 			</h1>
